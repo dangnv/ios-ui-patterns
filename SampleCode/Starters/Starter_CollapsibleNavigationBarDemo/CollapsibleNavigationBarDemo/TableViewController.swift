a125d7945853aa0a6acc54fcb6edca2089131163
clintcabanero <clint@bigleafmobile.com>
2016-03-11 05:54:46 -0800

    adding starter for collapsible nav bar

---
+++ SampleCode/Starters/Starter_CollapsibleNavigationBarDemo/CollapsibleNavigationBarDemo/TableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewController.swift
+//  CollapsibleNavigationBarDemo
+//
+//  Created by Clint Cabanero on 3/10/16.
+//  Copyright © 2016 ClintCabanero. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
